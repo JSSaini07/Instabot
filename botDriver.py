b305a9f2b8b86a1518ebd6faa6c4b8d148f5ca7c
Jasmeet Singh Saini <mr.jssaini07@gmail.com>
2019-06-19 22:46:34 +0530

    update bot driver

--- botDriver.py
+++ botDriver.py
@@ -11,10 +11,12 @@ class BotDriver:
     self.totalFollowing = self.instabot.get_total_following()
     self.maxFollowing = 500 
     self.sleeptime = 3600
+    self.tick = 0
   def begin(self):
     while(True):
       followedAccounts = False
       self.instabot.unfollow_accounts(self.unfollowTime)
+      self.totalFollowing = self.instabot.get_total_following()
       self.instabot.logger.log("Current following count {}".format(self.totalFollowing))
       if self.totalFollowing <= self.maxFollowing:
         self.instabot.begin_following(seedAccount = SEED_ACCOUNTS[int(random.random()*1000000) % len(SEED_ACCOUNTS)])
@@ -24,4 +26,8 @@ class BotDriver:
       if not followedAccounts:
         self.instabot.logger.log("Didn't follow anything in this iteration, sleeping for {} minutes".format(int(self.sleeptime/60)))
         time.sleep(self.sleeptime)
+      self.tick = self.tick + 1
+      if self.tick % 5 == 0:
+        self.instabot.logger.log("Iteration {}, updating data".format(self.tick))
+        self.instabot.fetch_following()
       self.totalFollowing = self.instabot.get_total_following()
